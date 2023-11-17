96c16e00327f9bd21a7e4a6ccd2bc00e28c92a1a
amvrosi <mr.vanya.sobol@gmail.com>
2023-11-17 16:04:29 +0100

    Dystrybucja 1 z javadoc.

--- src/main/java/pwo/app/Sequence.java
+++ src/main/java/pwo/app/Sequence.java
@@ -4,16 +4,19 @@
  */
 package pwo.app;
 
-
-
+/**
+ *
+ * @author ivan
+ */
 public class Sequence {
 
+    /**
+     * Metoda główna programu.
+     * 
+     * @param args Argumenty przekazywane do programu.
+     */
     public static void main(String[] args) {
-        switch(args.length){
-        case 3: (new SeqToOutApp()).run(args); break;
-        case 4: (new SeqToFileApp()).run(args); break;
-        default: System.out.println("!Illegal arguments\n"
-          + "Legal usage: seqName from to [fileName]");
+
+        (new SeqToFileApp()).run(args);
     }
-}
 }
